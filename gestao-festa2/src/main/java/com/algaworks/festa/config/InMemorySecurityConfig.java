4550f81875dd15591f7753455971134935292706
eidrianxd123 <carloshenriquearantes@hotmail.com>
2021-03-19 21:24:13 -0300

    first commit

---
+++ gestao-festa2/src/main/java/com/algaworks/festa/config/InMemorySecurityConfig.java
@@ -0,0 +1,10 @@
+package com.algaworks.festa.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
+@Configuration
+public class InMemorySecurityConfig {
+
+}
